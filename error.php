24be78e0fdc6656b5bb72410b9861993ee03fdfc
Andrew Belleza <96896906+Da-drew@users.noreply.github.com>
2022-03-23 14:35:15 +0800

    Delete error.php

--- error.php
+++
@@ -1,9 +0,0 @@
-<?php include 'includes/main-header.php'?>
-
-<div class="response-div text-center p-5">
-    <h1 class="response-title">Error has been occur.</h1>
-    <p class="response-sub-title">Please check your internet connection and try again. Thank you.</p>
-</div>
-
-<?php include 'includes/modal.php' ?>
-<?php include 'includes/footer.php' ?>
